82f95542450d6642b7002345c07a7ca0399d2d71
poklipart <angellopezandrade4@gmail.com>
2020-01-20 15:16:36 +1100

    styleless login page component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Login extends Component {
     render() {
         return(
             <>
-                <h1>Chemical Auditor</h1>
-                <h2>Login</h2>
+                <div id="header-bar">
+                    <h1>Chemical Auditor</h1>
+                </div>
+                <div id="sign-in-register-box">
+                    <h2>Sign In</h2>
+                    <form>
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" />
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" />
+                        <input type="submit" value="Sign in" />
+                    </form>
+                    <Link to="/register">Register New Account</Link>
+                </div>
             </>
         )
     }
