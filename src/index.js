3f6bee3c66b400b859eb0dd3637ed8e1fa41a758
maxPiroddi <mpiroddi@outlook.com>
2020-01-23 12:36:29 +1100

    html material-ui meta tags

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./store";
-import 'typeface-roboto';
+import "typeface-roboto";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById("root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
