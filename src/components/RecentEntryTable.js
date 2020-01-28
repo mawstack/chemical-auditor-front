84a16b168ba7899cacd632756abba53c13ab0220
waynezdev <cas021919@coderacademy.edu.au>
2020-01-28 14:09:44 +1100

    updated table format and heading

--- src/components/RecentEntryTable.js
+++ src/components/RecentEntryTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,28 +8,46 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-const styles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+const StyledTableCell = withStyles(theme => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+  const StyledTableRow = withStyles(theme => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.background.default,
+      },
+    },
+  }))(TableRow);
+  
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData('Monday', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+  
+  const styles = makeStyles({
+    table: {
+      minWidth: 700,
+    },
+  });
 
 class DenseTable extends Component {
 
     state = {
- 
+    date:"",    
     startTime: 0,
     finishTime: 0,
     cropRow: 0,
@@ -51,24 +69,24 @@ class DenseTable extends Component {
             <Table className={styles.table} size="small" aria-label="a dense table">
                 <TableHead>
                 <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    <StyledTableCell>Date </StyledTableCell>
+                    <StyledTableCell align="right">Starting time</StyledTableCell>
+                    <StyledTableCell align="right">Finish time</StyledTableCell>
+                    <StyledTableCell align="right">Name</StyledTableCell>
+                    {/* <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell> */}
                 </TableRow>
                 </TableHead>
                 <TableBody>
                 {rows.map(row => (
-                    <TableRow key={row.name}>
-                    <TableCell component="th" scope="row">
+                    <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
                         {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                    {/* <StyledTableCell align="right">{row.protein}</StyledTableCell> */}
+                    </StyledTableRow>
                 ))}
                 </TableBody>
             </Table>
@@ -78,3 +96,4 @@ class DenseTable extends Component {
 }
 
 export default DenseTable
+
